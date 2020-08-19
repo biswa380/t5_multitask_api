079c4f2f1c3e337762460bcb4b5662ff60556e5f
Biswabhusan Pradhan <34735768+biswa380@users.noreply.github.com>
2020-08-19 13:11:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,6 +26,25 @@ def generateAnswer(context, question):
     "question": question,
     "context": context
     })
+model = T5ForConditionalGeneration.from_pretrained('t5-small')
+tokenizer = T5Tokenizer.from_pretrained('t5-small')
+device = torch.device('cpu')
 
+@app.route("/summarize/<text>") 
+def extractSummary(text):
+    preprocess_text = text.strip().replace("\n","")
+    t5_prepared_Text = "summarize: "+preprocess_text
+    tokenized_text = tokenizer.encode(t5_prepared_Text, return_tensors="pt").to(device)
+    summary_ids = model.generate(tokenized_text,
+                                    num_beams=4,
+                                    no_repeat_ngram_size=2,
+                                    min_length=100,
+                                    max_length=150,
+                                    early_stopping=True)
+    output = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+    print('******************** Summary *********************')
+    print(output)
+    print('*********************************************')
+    return output
     
     
